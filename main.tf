1ae9840a91b97e2b7860f444300ff8f54217b630
WesamKhalil <wkhalil@tutamail.com>
2024-03-27 00:44:03 +0000

    updated terraform to use hosted zone and load balancer

--- main.tf
+++ main.tf
@@ -16,10 +16,13 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
+    region = var.region
 }
 
-resource "aws_security_group" "example" {
+resource "aws_security_group" "blog_web_server_sg" {
+
+  name = "tf-blog-web-server-sg"
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -43,16 +46,77 @@ resource "aws_security_group" "example" {
   }
 }
 
-resource "aws_key_pair" "personal" {
-  key_name   = "personal"
+resource "aws_key_pair" "personal_kp" {
+  key_name   = "tf-personal-kp"
   public_key = var.aws-personal_security_key
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "blog_web_server_instance" {
     ami = var.ubuntu_ami
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.example.name]
-    key_name = aws_key_pair.personal.key_name
+    security_groups = [aws_security_group.blog_web_server_sg.name]
+    key_name = aws_key_pair.personal_kp.key_name
+    availability_zone = local.availability_zones[0]
 
     user_data = var.docker_blog_app_setup
+
+    tags = {
+      Name = "tf-blog-web-server-instance"
+    }
+}
+
+resource "aws_default_vpc" "default" {
+}
+
+resource "aws_default_subnet" "default_us_east_1a" {
+  availability_zone = local.availability_zones[0]
+}
+
+resource "aws_default_subnet" "default_us_east_1b" {
+  availability_zone = local.availability_zones[1]
+}
+
+resource "aws_lb_target_group" "blog_web_server_lb_tg" {
+  name     = "tf-blog-web-server-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
+
+resource "aws_lb_target_group_attachment" "blog_web_server_lb_tga" {
+ target_group_arn = aws_lb_target_group.blog_web_server_lb_tg.arn
+ target_id        = aws_instance.blog_web_server_instance.id
+ port             = 80
+}
+
+resource "aws_lb" "blog_web_server_lb" {
+  name               = "tf-blog-web-server-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.blog_web_server_sg.id]
+  subnets            = [aws_default_subnet.default_us_east_1a.id, aws_default_subnet.default_us_east_1b.id]
+}
+
+resource "aws_lb_listener" "blog_web_server_lb_listener" {
+ load_balancer_arn = aws_lb.blog_web_server_lb.arn
+ port              = "80"
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.blog_web_server_lb_tg.arn
+ }
+}
+
+
+resource "aws_route53_record" "www" {
+  zone_id = var.wesamkhalildotuk_zone_id
+  name    = "www.wesamkhalil.uk"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.blog_web_server_lb.dns_name
+    zone_id                = aws_lb.blog_web_server_lb.zone_id
+    evaluate_target_health = true
+  }
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,12 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+locals {
+ availability_zones = ["${var.region}a", "${var.region}b"]
+}
+
 variable "aws-personal_security_key" {
     type = string
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCPOYg3ysyJemNb/6LdR8pJ1eCUE193exkxJ5DjAq08o+IrbwfvaoPemQnGHLRaTTXZKrR9QzclZN0WbYgqHImnj5ANOBflVPqr8a+QdoP7Z8NAEQrYv6Es7HPUVDp61rFCVLS/WZu1pP3HiuAw4xhennIRvB0UunfCSlCwirRnpECcVfwSoPqS+u/YrBpBy8ytAN7P8KYwE0FHdm5XKoPee918aD+SejxzxVgE2dkDFDx8wgrAHdhhrvArcuc9GkzEj7Rji5D6DlUTXQHy3C9/zEGlE3akpGc7U3/3ROoTf77dmPXHOjkoCnMIsdbR9QO+s8n/fvHhVi/I4u4XpJNI/QgOoYTehHrNeCSgBm8JyYt2GDYdWqjtPrzJhiq7fWN98rUmmLJIx54FRmuTrUxMZ8GW8ZedLIvNGhpdAZpBUcUZyvtIBGCOR0pmJcQ2UnATIJo8dbzzAqr9uHv8laBPOIyDUd9QxsNzjoZBwn7XkcllFDAVh5K8Oil6EXdGrM8= john@john-MS-7C56"
@@ -30,4 +39,9 @@ variable "docker_blog_app_setup" {
 
       docker run -d -p 80:3000 wesamkhalil/blog-app:v1
     EOL
+}
+
+variable "wesamkhalildotuk_zone_id" {
+  type = string
+  default = "Z01064701HUUN21JB8C76"
 }
